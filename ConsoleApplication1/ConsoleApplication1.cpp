0c31aa3587c8877c88791abd1ad59bd1bed47177
Tanasa Stefan-Alexandru <stefan_tanasa2004@yahoo.com>
2025-02-03 16:20:36 +0200

    Initial commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "portaudio.h"
+
+int main() {
+    PaError err = Pa_Initialize();
+    if (err != paNoError) {
+        std::cerr << "PortAudio error: " << Pa_GetErrorText(err) << std::endl;
+        return 1;
+    }
+
+    std::cout << "PortAudio initialized successfully!" << std::endl;
+
+    Pa_Terminate();
+    return 0;
+}
